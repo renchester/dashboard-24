3cdfb353958ebdb894e2dc23991f7d6088a0381d
Renchester Ramos <renchesterjramos@gmail.com>
2024-09-23 20:08:44 +0800

    Remove boilerplate code and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Providers from './Providers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Providers>
+      <div className="app">
+        <div>
+          <div>
+            <img src="/profile.jpeg" alt="Profile picture" />
+          </div>
+          <p>Hi Renchester!</p>
+
+          <p>Username: </p>
+        </div>
+      </div>
+    </Providers>
   );
 }
 
