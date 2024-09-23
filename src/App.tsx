9616efb0a4baa7c5e4c0b21f6dfd80152acc4664
Renchester Ramos <renchesterjramos@gmail.com>
2024-09-24 03:42:26 +0800

    Implement components into main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Providers from './Providers';
 import ProfileCard from './components/ProfileCard';
-import Switch from './components/shared/Switch';
 import ActivityList from './components/ActivityList';
 import StatisticsGraph from './components/StatisticsGraph';
 import SettingsPanel from './components/SettingsPanel';
@@ -16,21 +15,6 @@ export type UserDetails = {
 };
 
 function App() {
-  const [userDetails, setUserDetails] = useState<UserDetails>({
-    firstName: 'Renchester',
-    lastName: 'Ramos',
-    username: 'renchester',
-    title: 'Full-stack Developer',
-    email: 'renchesterjramos@gmail.com',
-    bio: 'A full-stack web developer with a background in architecture, he has a passion for creating smooth experiences for the end-user.',
-  });
-  const handleDetailsChange = (name: string, value: string) => {
-    setUserDetails((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
   const [isDark, setIsDark] = useState(true);
   const [isEmailOn, setEmailStatus] = useState(false);
   const toggleDarkTheme = () => setIsDark((currentTheme) => !currentTheme);
@@ -41,39 +25,19 @@ function App() {
     <Providers>
       <div className={`app ${isDark ? 'dark-mode' : ''}`}>
         <div className="top">
-          <h1 className="top__title">Dashboard</h1>
-          <form action="" className="top__settings">
-            <div className="top__settingsBox">
-              <label htmlFor="switch_theme" className="top__settingsLabel">
-                Dark Mode:
-              </label>
-              <Switch
-                isOn={isDark}
-                id="switch_theme"
-                handler={toggleDarkTheme}
-              />
-            </div>
-            <div className="top__settingsBox">
-              <label htmlFor="switch_email" className="top__settingsLabel">
-                Email Notifications:
-              </label>
-              <Switch
-                isOn={isEmailOn}
-                id="switch_email"
-                handler={toggleEmailStatus}
-              />
-            </div>
-          </form>
+          <h1>Dashboard</h1>
+          <SettingsPanel
+            isDark={isDark}
+            toggleDarkTheme={toggleDarkTheme}
+            isEmailOn={isEmailOn}
+            toggleEmailStatus={toggleEmailStatus}
+          />
         </div>
         <div className="main">
           {/* GRID */}
-          <ProfileCard details={userDetails} />
+          <ProfileCard />
           <ActivityList />
           <StatisticsGraph />
-          {/* <SettingsPanel
-            handleDetailsChange={handleDetailsChange}
-            userDetails={userDetails}
-          /> */}
         </div>
       </div>
     </Providers>
