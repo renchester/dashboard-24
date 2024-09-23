deceb08abd1c47335ca46a856224c3825b306f1c
Renchester Ramos <renchesterjramos@gmail.com>
2024-09-24 03:41:59 +0800

    Create Graphs component

--- src/components/StatisticsGraph.tsx
+++ src/components/StatisticsGraph.tsx
@@ -12,6 +12,7 @@ import {
   PointElement,
 } from 'chart.js';
 import { eachDayOfInterval, format, subDays } from 'date-fns';
+import { _DeepPartialObject } from 'chart.js/dist/types/utils';
 
 ChartJS.register(
   CategoryScale,
@@ -42,6 +43,7 @@ function StatisticsGraph() {
         backgroundColor: 'rgba(75, 192, 192, 0.6)',
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 1,
+        borderRadius: 5,
       },
     ],
   };
@@ -52,54 +54,59 @@ function StatisticsGraph() {
       {
         label: 'Page Views',
         data: [609, 773, 954, 639, 772, 369, 965],
-        fill: false,
+        fill: true,
         borderColor: 'rgba(75, 192, 192, 1)',
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        tension: 0.1,
+        tension: 0.3,
       },
       {
         label: 'Error Logs',
         data: [8, 9, 10, 5, 4, 6, 10],
-        fill: false,
+        fill: true,
         borderColor: 'rgba(255, 99, 132, 1)',
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        tension: 0.1,
+        tension: 0.3,
       },
     ],
   };
 
-  const barGraphOptions = {
-    scales: {
-      y: {
-        beginAtZero: true,
-        stepSize: 1,
-      },
-    },
-  } as const;
-
-  const lineGraphOptions = {
-    scales: {
-      y: {
-        beginAtZero: true,
-        stepSize: 50,
-        title: {
-          display: true,
-          text: 'Count',
-        },
-      },
-    },
-  };
-
   return (
-    <div>
-      <div>
-        <h2>Recent Commits</h2>
-        <Bar data={commitData} options={barGraphOptions} />
+    <div className="card card--graph graph">
+      <div className="graph__box graph__analytics">
+        <h2>Web Analytics and Error Logs</h2>
+        <Line
+          data={analyticsData}
+          options={{
+            responsive: true,
+            scales: {
+              y: {
+                beginAtZero: true,
+                title: {
+                  display: true,
+                  text: 'Page Views',
+                },
+              },
+            },
+          }}
+        />
       </div>
-
-      <div>
-        <h2>Web Analytics and Error Logs (Past Week)</h2>
-        <Line data={analyticsData} options={lineGraphOptions} />
+      <div className="graph__box graph__commit">
+        <h2>Recent Commits</h2>
+        <Bar
+          data={commitData}
+          options={{
+            responsive: true,
+            scales: {
+              y: {
+                beginAtZero: true,
+                title: {
+                  display: true,
+                  text: 'No. of Commits',
+                },
+              },
+            },
+          }}
+        />
       </div>
     </div>
   );
